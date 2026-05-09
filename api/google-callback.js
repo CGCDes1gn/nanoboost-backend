d2a140c1331a7d29c91cc70dfb1ef34a61b5a2a0
CGCDes1gn <crisgraphiccorner@gmail.com>
2026-05-09 15:32:55 -0500

    Update google-callback.js

--- api/google-callback.js
+++ api/google-callback.js
@@ -27,6 +27,8 @@ export default async function handler(req, res) {
     }
 
     // ✅ guardar en Supabase
+    console.log("SUPABASE_URL:", process.env.SUPABASE_URL);
+    console.log("TIENE_SERVICE_KEY:", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
     await fetch(`${process.env.SUPABASE_URL}/rest/v1/google_tokens`, {
       method: "POST",
       headers: {
