2ca4273707b2e0859c530b1d3eb53e8a992278c8
CGCDes1gn <crisgraphiccorner@gmail.com>
2026-05-09 09:02:42 -0500

    Update vertex-generate-image.js

--- api/vertex-generate-image.js
+++ api/vertex-generate-image.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     const response = await fetch(url, {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${process.env.GCP_ACCESS_TOKEN}`,
+        "Authorization": `Bearer ${process.env.GCP_ACCESS_TOKEN.trim()}`,
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
